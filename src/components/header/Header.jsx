e070b72a9b1ba48b47f5f65003cf89b6e059fa1c
David2261 <bulatnasirov2003@gmail.com>
2022-11-06 17:29:11 +0300

    Complete header and add react-icons package to node

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import CTA from "./CTA"
 import './header.css'
+import ME from "../../assets/Bulat_Moscow.webp"
+import HeaderSocial from "./HeaderSocial";
 
 
 const Header = () => {
 	return (
-		<div>Header</div>
+		<header>
+			<div className="container header__container">
+				<h5>Hello I'm</h5>
+				<h1>Bulat Nasyrov</h1>
+				<h5 className="text-light">Back-end Developer</h5>
+				<CTA />
+				<HeaderSocial />
+
+				<div className="me">
+					<img src={ME} alt="me" />
+				</div>
+
+				<a href="#contact" className="scroll__down">Srcoll Down</a>
+			</div>
+		</header>
 	)
 }
 
