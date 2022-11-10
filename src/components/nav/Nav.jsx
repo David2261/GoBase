ac1245d320346502aa96fda81b877ead9ae9f4ac
David2261 <bulatnasirov2003@gmail.com>
2022-11-11 00:02:51 +0300

    Add navbar with used useState hook

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './nav.css'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai';
+import {GiMaterialsScience} from 'react-icons/gi';
+import {BiWorld, BiMessageRoundedDetail} from 'react-icons/bi';
+
+const [activeNav, setActiveNav] = useState('#');
 
 
 const Nav = () => {
 	return (
-		<div>Nav</div>
+		<nav>
+			<a href="#" onClick={() => setActivateNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+			<a
+				href="#about"
+				onClick={() => setActivateNav('#about')}
+				className={activeNav === '#about' ? 'active' : ''}
+			><AiOutlineUser /></a>
+			<a href="#experience"
+				onClick={() => setActivateNav('#experience')}
+				className={activeNav === '#experience' ? 'active' : ''}
+			><GiMaterialsScience /></a>
+			<a href="#services"
+				onClick={() => setActivateNav('#services')}
+				className={activeNav === '#services' ? 'active' : ''}
+			><BiWorld /></a>
+			<a href="#contact"
+				onClick={() => setActivateNav('#contact')}
+				className={activeNav === '#contact' ? 'active' : ''}
+			><BiMessageRoundedDetail /></a>
+		</nav>
 	)
 }
 
 
-export default Nav
+export default Nav;
