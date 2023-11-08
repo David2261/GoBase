0708e9aa11c28c28b93c247040e654e54546b18c
root <root@Bulat>
2023-11-08 22:11:50 +0300

    Edu var and func print and scan

--- hello.go
+++ hello.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	var alert string = "What's you name?"
+	var name string
+	var age uint8
+	fmt.Println(alert)
+	fmt.Scan(&name)
+	fmt.Printf("Hello " + name + "!\n")
+	fmt.Printf("How old are you?\n")
+	fmt.Scan(&age)
+	fmt.Printf("That is great " + fmt.Sprint(age) + " years\n")
 }
